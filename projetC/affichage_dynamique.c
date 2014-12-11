1c0bc0ff3cb29c8de55f9f5acde2f8d13f90e80f
Marthin <martin.guilloux@gmail.com>
2014-12-11 09:27:55 +0100

    affichage dynamique

---
+++ projetC/affichage_dynamique.c
@@ -0,0 +1,17 @@
+ouleur(); /* change la couleur du texte (couleur de 30 a 47) */
+void bell (int n); /* je sais pas */
+void bold (); /* écrit en gras */
+void clreol (); /* efface la ligne */
+void clrscreen (); /* efface toute la console */
+void cursoff (); /* dés-affiche le curseur (le truc qui clignote) */
+void curson  (); /* affiche le curseur */
+void home (); /* je sais pas */
+void insert (); /* je sais pas */
+void normal (); /* écrit normalement */
+void poscur (int l,int c); /* positionne le curseur, probablement ligne, colonne */
+void replace (); /* je sais pas */
+void underline (); /* souligne */
+void videoff (); /* je sais pas */
+void videon (); /* je sais pas */
+void blink (); /* je sais pas */
+ 
