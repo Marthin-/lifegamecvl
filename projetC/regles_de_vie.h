a469656715933d0cbc931462aad5a48e42f1bc97
Constant Thibaut <tconstan@SA207-01.ensi-bourges.fr>
2015-01-09 12:13:09 +0100

    creation reproduction2

--- projetC/regles_de_vie.h
+++ projetC/regles_de_vie.h
@@ -7,8 +7,11 @@ void killObj(obj *tab, int place);
 void survie(obj *tab, int n, int tour);
 int apte(obj *tab, int place, int tour);
 int isPresent(obj *tab, int place, int n, int nbType);
+int isPresent2(obj *tab, int place, int n, int nbType);
+int returnPlace(int is, int place, int n);
 obj getSameType(obj inc, int tour);
 void reproduction(obj *tab, int n, int tour);
+void reproduction2(obj *tab, int n, int tour);
 
 void predation(obj *tab, int n, int tour);
 void deplacement(obj *tab, int n, int tour);
